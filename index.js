0eee0a5cb941b67f2e1a482f02fd895e886c34bd
DhirajRathore <dhirajs8296@gmail.com>
2023-11-06 17:19:26 +0530

    server final changes
    server final changes

---
+++ index.js
@@ -0,0 +1,30 @@
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors({ origin: "http://localhost:3000" }));
+
+const httpServer = createServer();
+const io = new Server(httpServer, {
+  cors: "http://localhost:3000",
+});
+
+io.on("connection", (socket) => {
+  console.log("server connected");
+
+  socket.on("beginPath", (arg) => {
+    socket.broadcast.emit("beginPath", arg);
+  });
+
+  socket.on("drawLine", (arg) => {
+    socket.broadcast.emit("drawLine", arg);
+  });
+
+  socket.on("changeConfig", (arg) => {
+    socket.broadcast.emit("changeConfig", arg);
+  });
+});
+
+httpServer.listen(5000);
